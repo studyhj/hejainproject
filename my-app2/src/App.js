1d5b9945614bfb06b275269d229750a0010bd2d1
YDBJWB0038 <17610725053@163.com>
2018-06-28 13:44:44 +0800

    时钟

--- my-app2/src/App.js
+++ my-app2/src/App.js
@@ -3,18 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(){
+        super()
+        this.state={
+            date:new Date()
+        }
+    }
+   componentWillMount(){
+        this.timer=setInterval(()=>{
+            this.setState({
+                date:new Date()
+            })
+        },1000)
+
+   }
+    componentWillUnmount () {
+        clearInterval(this.timer)
+    }
   render() {
     return (
+        <div>
+            <h1>
+                <p>
+                    现在的时间是
+                </p>
+                {this.state.date.toLocaleTimeString()}
+            </h1>
+        </div>
 
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
--- my-app2/src/index.js
+++ my-app2/src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Index extends Component{
+    constructor(){
+        super()
+        this.state={
+            isShowClick:true
+        }
+    }
+    handleShowOrHide(){
+        this.setState({
+            isShowClick:!this.state.isShowClick
+        })
+    }
+    render(){
+        return(
+            <div>
+                {this.state.isShowClick? <App />:null}
+                <button onClick={this.handleShowOrHide.bind(this)}>显示或隐藏时钟</button>
+            </div>
+
+        )
+    }
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
 registerServiceWorker();
