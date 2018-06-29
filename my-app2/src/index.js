b80b54b38dbc4a87a7d89373e954d27bf272a181
YDBJWB0038 <17610725053@163.com>
2018-06-29 09:58:17 +0800

    readme

--- my-app2/src/index.js
+++ my-app2/src/index.js
@@ -1,31 +1,8 @@
-import React,{Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import CommentApp from './CommentApp';
+import './index.css'
 
-class Index extends Component{
-    constructor(){
-        super()
-        this.state={
-            isShowClick:true
-        }
-    }
-    handleShowOrHide(){
-        this.setState({
-            isShowClick:!this.state.isShowClick
-        })
-    }
-    render(){
-        return(
-            <div>
-                {this.state.isShowClick? <App />:null}
-                <button onClick={this.handleShowOrHide.bind(this)}>显示或隐藏时钟</button>
-            </div>
 
-        )
-    }
-}
-
-ReactDOM.render(<Index />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<CommentApp />, document.getElementById('root'));
+//registerServiceWorker();
